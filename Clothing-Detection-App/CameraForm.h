b9c4c4df6bc31f6c57d2c29007ccee2817c1cb8e
Kakuren <klong8257@gmail.com>
2025-12-28 14:32:56 +0700

    Add ToolStripContainer and SplitContainer to CameraForm
    Introduced ToolStripContainer and SplitContainer as private members of CameraForm. Updated InitializeComponent to set up docking, layout, and control hierarchy for improved UI structure and future extensibility.

--- Clothing-Detection-App/CameraForm.h
+++ Clothing-Detection-App/CameraForm.h
@@ -34,6 +34,9 @@ namespace ClothingDetectionApp {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::ToolStripContainer^ toolStripContainer1;
+	protected:
+	private: System::Windows::Forms::SplitContainer^ splitContainer1;
 
 	private:
 		/// <summary>
@@ -48,15 +51,50 @@ namespace ClothingDetectionApp {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->toolStripContainer1 = (gcnew System::Windows::Forms::ToolStripContainer());
+			this->splitContainer1 = (gcnew System::Windows::Forms::SplitContainer());
+			this->toolStripContainer1->ContentPanel->SuspendLayout();
+			this->toolStripContainer1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->BeginInit();
+			this->splitContainer1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// toolStripContainer1
+			// 
+			// 
+			// toolStripContainer1.ContentPanel
+			// 
+			this->toolStripContainer1->ContentPanel->Controls->Add(this->splitContainer1);
+			this->toolStripContainer1->ContentPanel->Size = System::Drawing::Size(1194, 494);
+			this->toolStripContainer1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->toolStripContainer1->Location = System::Drawing::Point(0, 0);
+			this->toolStripContainer1->Name = L"toolStripContainer1";
+			this->toolStripContainer1->Size = System::Drawing::Size(1194, 519);
+			this->toolStripContainer1->TabIndex = 0;
+			this->toolStripContainer1->Text = L"toolStripContainer1";
+			// 
+			// splitContainer1
+			// 
+			this->splitContainer1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->splitContainer1->Location = System::Drawing::Point(0, 0);
+			this->splitContainer1->Name = L"splitContainer1";
+			this->splitContainer1->Size = System::Drawing::Size(1194, 494);
+			this->splitContainer1->SplitterDistance = 973;
+			this->splitContainer1->TabIndex = 0;
 			// 
 			// CameraForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1194, 519);
+			this->Controls->Add(this->toolStripContainer1);
 			this->Name = L"CameraForm";
 			this->Text = L"CameraForm";
+			this->toolStripContainer1->ContentPanel->ResumeLayout(false);
+			this->toolStripContainer1->ResumeLayout(false);
+			this->toolStripContainer1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->EndInit();
+			this->splitContainer1->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
